196495f5b7fa4f97377a43931c2e8a4c0c5d7bea
anton-phipps <96834533+anton-phipps@users.noreply.github.com>
2025-05-30 11:42:42 -0600

    added more visual styles

--- init.lua
+++ init.lua
@@ -846,7 +846,11 @@ require('lazy').setup({
       }
     end,
   },
-
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+  },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -859,7 +863,8 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'tokyonight-moon'
+      -- vim.cmd.colorscheme 'tokyonight-moon'
+      vim.cmd.colorscheme 'catppuccin-latte' -- catppuccin, catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
